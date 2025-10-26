bf5f7056d7f3d92d3728c80e3709603a7d9e1d33
Ncoreo <ilya.pvl@icloud.com>
2025-10-26 23:14:41 +0300

    Update OptimalCache.h

--- include/OptimalCache.h
+++ include/OptimalCache.h
@@ -13,7 +13,7 @@ private:
     size_t cache_size;
     std::function<int(int)> slow_get_page_;
     std::unordered_map<int, std::queue<size_t>> future_indices;
-    std::unordered_map<int, int> cache_values; // Store actual values
+    std::unordered_map<int, int> cache_values;
 
 public:
     OptimalCache(size_t size, std::function<int(int)> slow_get_page);
