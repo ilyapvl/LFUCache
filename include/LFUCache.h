f2e65b11cc9c307560f640421378db32616af652
Ncoreo <ilya.pvl@icloud.com>
2026-01-30 18:42:38 +0300

    Update LFUcache

--- include/LFUCache.h
+++ include/LFUCache.h
@@ -12,118 +12,122 @@
 #include <iostream>
 
 #include "global.h"
-#include "exceptions/InvalidArgumentException.h"
 #include "exceptions/CacheOperationException.h"
 
-/**
- * @brief LFU кэш
- * 
- * @tparam K Тип ключа
- * @tparam V Тип значения
- */
-template<typename K, typename V>
-class LFUCache
+namespace lfu
 {
-private:
     /**
-     * @brief Структура узла кэша
+     * @brief LFU кэш
+     * 
+     * @tparam K Тип ключа
+     * @tparam V Тип значения
      */
-    struct Node
+    template<typename K, typename V>
+    class LFUCache
     {
-        K key;      
-        V value;    
-        int frequency;  
+    private:
+        /**
+         * @brief Структура узла кэша
+         */
+        struct Node
+        {
+            K key;      
+            V value;    
+            int frequency;  
+            
+            /**
+             * @brief Конструктор узла
+             * @param k Ключ
+             * @param v Значение
+             * @param f Начальная частота
+             */
+            Node(const K& k, const V& v, int f) : key(k), value(v), frequency(f)
+            {}
+        };
+        
+        using NodeIterator = typename std::list<Node>::iterator;
+        using SlowGetFunc = std::function<V(K)>;
+        
+        size_t capacity_;          
+        int min_frequency_;        
+        SlowGetFunc slow_get_func_;
         
         /**
-         * @brief Конструктор узла
-         * @param k Ключ
-         * @param v Значение
-         * @param f Начальная частота
+         * @brief Карта частот т. е. список элементов с данной частотой
          */
-        Node(const K& k, const V& v, int f) : key(k), value(v), frequency(f)
-        {}
+        std::unordered_map<int, std::list<Node>> frequency_map_;
+        
+        /**
+         * @brief Карта ключей - список итераторов на элементы
+         */
+        std::unordered_map<K, NodeIterator> key_map_;
+
+        /**
+         * @brief Увеличивает частоту использования элемента
+         * @param it Итератор на элемент в списке
+         * 
+         * @throws CacheOperationException если элемент не найден в структурах данных
+         */
+        void increase_frequency(NodeIterator it);
+
+    public:
+        /**
+         * @brief Конструктор кэша
+         * @param capacity Вместимость кэша >0
+         * @param slow_get_func Функция для медленного получения значения
+         * 
+         * @throws std::invalid_argument если capacity <= 0
+         */
+        LFUCache(size_t capacity, SlowGetFunc slow_get_func);
+        
+        ~LFUCache() noexcept = default;
+
+        /**
+         * @brief Получить значение по ключу
+         * @param key Ключ
+         * @return Ссылка на значение
+         * 
+         * @throws std::out_of_range если ключ не найден
+         * @throws CacheOperationException если ошибка операции
+         */
+        V& get(const K& key);
+        
+        /**
+         * @brief Поместить значение в кэш
+         * @param key Ключ
+         */
+        void put(const K& key);
+        
+        /**
+         * @brief Вытеснить один элемент из кэша
+         * @throws CacheOperationException если кэш пуст
+         */
+        void evict();
+        
+        /**
+         * @brief Получить текущий размер кэша
+         * @return Количество элементов в кэше
+         */
+        size_t size() const;
+        
+        /**
+         * @brief Проверить, пуст ли кэш
+         * @return true если кэш пуст иначе false
+         */
+        bool empty() const;
+        
+        /**
+         * @brief Получить вместимость кэша
+         * @return Макс количество элементов
+         */
+        size_t capacity() const;
+        
+        /**
+         * @brief Очистить кэш
+         */
+        void clear();
     };
-    
-    using NodeIterator = typename std::list<Node>::iterator;
-    using SlowGetFunc = std::function<V(K)>;
-    
-    size_t capacity_;          
-    int min_frequency_;        
-    SlowGetFunc slow_get_func_;
-    
-    /**
-     * @brief Карта частот т. е. список элементов с данной частотой
-     */
-    std::unordered_map<int, std::list<Node>> frequency_map_;
-    
-    /**
-     * @brief Карта ключей - список итераторов на элементы
-     */
-    std::unordered_map<K, NodeIterator> key_map_;
-
-    /**
-     * @brief Увеличивает частоту использования элемента
-     * @param it Итератор на элемент в списке
-     * 
-     * @throws CacheOperationException если элемент не найден
-     */
-    void increase_frequency(NodeIterator it);
-
-public:
-    /**
-     * @brief Конструктор кэша
-     * @param capacity Вместимость кэша >0
-     * @param slow_get_func Функция для медленного получения значения
-     * 
-     * @throws InvalidArgumentException если capacity <= 0
-     */
-    LFUCache(size_t capacity, SlowGetFunc slow_get_func);
-    
-    /**
-     * @brief Получить значение по ключу
-     * @param key Ключ
-     * @return Ссылка на значение
-     * 
-     * @throws std::out_of_range если ключ не найден
-     * @throws CacheOperationException если ошибка операции
-     */
-    V& get(const K& key);
-    
-    /**
-     * @brief Поместить значение в кэш
-     * @param key Ключ
-     */
-    void put(const K& key);
-    
-    /**
-     * @brief Вытеснить один элемент из кэша
-     * @throws CacheOperationException если кэш пуст
-     */
-    void evict();
-    
-    /**
-     * @brief Получить текущий размер
-     * @return Количество элементов
-     */
-    size_t size() const;
-    
-    /**
-     * @brief Проверить, пуст ли кэш
-     * @return true если кэш пуст иначе false
-     */
-    bool empty() const;
-    
-    /**
-     * @brief Получить вместимость кэша
-     * @return Макс количество элементов
-     */
-    size_t capacity() const;
-    
-    /**
-     * @brief Очистить кэш
-     */
-    void clear();
-};
+}
 
 #include "LFUCache.tpp"
 
--- src/LFUCache.tpp
+++ src/LFUCache.tpp
@@ -9,16 +9,16 @@
 #include "LFUCache.h"
 #include <stdexcept>
 
-template<typename K, typename V>
-void LFUCache<K, V>::increase_frequency(NodeIterator it)
+template <typename K, typename V>
+void lfu::LFUCache<K, V>::increase_frequency(NodeIterator it)
 {
     if (it == NodeIterator())
     {
         throw CacheOperationException("Invalid iterator in increase_frequency");
     }
     
-    Node node = *it;
-    int old_freq = node.frequency;
+    Node& node_ref = *it;
+    int old_freq = node_ref.frequency;
     
     auto freq_it = frequency_map_.find(old_freq);
     if (freq_it == frequency_map_.end() || freq_it->second.empty())
@@ -26,6 +26,7 @@ void LFUCache<K, V>::increase_frequency(NodeIterator it)
         throw CacheOperationException("Problems with freq map");
     }
     
+    auto node = *it;
     freq_it->second.erase(it);
     
     if (freq_it->second.empty())
@@ -38,22 +39,22 @@ void LFUCache<K, V>::increase_frequency(NodeIterator it)
     }
     
     node.frequency++;
-    frequency_map_[node.frequency].push_front(node);
+    frequency_map_[node.frequency].push_front(std::move(node));
     key_map_[node.key] = frequency_map_[node.frequency].begin();
 }
 
 template<typename K, typename V>
-LFUCache<K, V>::LFUCache(size_t capacity, SlowGetFunc slow_get_func) 
+lfu::LFUCache<K, V>::LFUCache(size_t capacity, SlowGetFunc slow_get_func) 
     : capacity_(capacity), min_frequency_(0), slow_get_func_(std::move(slow_get_func))
 {
     if (capacity_ <= 0)
     {
-        throw InvalidArgumentException("Cache capacity must be greater than 0");
+        throw std::invalid_argument("Cache capacity must be greater than 0");
     }
 }
 
 template<typename K, typename V>
-V& LFUCache<K, V>::get(const K& key)
+V& lfu::LFUCache<K, V>::get(const K& key)
 {
     auto it = key_map_.find(key);
     if (it == key_map_.end())
@@ -66,7 +67,7 @@ V& LFUCache<K, V>::get(const K& key)
 }
 
 template<typename K, typename V>
-void LFUCache<K, V>::put(const K& key)
+void lfu::LFUCache<K, V>::put(const K& key)
 {
     if (capacity_ == 0)
     {
@@ -76,23 +77,25 @@ void LFUCache<K, V>::put(const K& key)
     auto it = key_map_.find(key);
     if (it != key_map_.end())
     {
-        it->second->value = slow_get_func_(key);
+        V new_value = slow_get_func_(key);
+        it->second->value = std::move(new_value);
         increase_frequency(it->second);
         return;
     }
     
+    V value = slow_get_func_(key);
     if (key_map_.size() >= capacity_)
     {
         evict();
     }
     
-    min_frequency_ = 1; //TODO - 1 или 0
+    min_frequency_ = 1;
     frequency_map_[1].push_front(Node(key, slow_get_func_(key), 1));
     key_map_[key] = frequency_map_[1].begin();
 }
 
 template<typename K, typename V>
-void LFUCache<K, V>::evict()
+void lfu::LFUCache<K, V>::evict()
 {
     if (empty())
     {
@@ -119,25 +122,25 @@ void LFUCache<K, V>::evict()
 }
 
 template<typename K, typename V>
-size_t LFUCache<K, V>::size() const
+size_t lfu::LFUCache<K, V>::size() const
 {
     return key_map_.size();
 }
 
 template<typename K, typename V>
-bool LFUCache<K, V>::empty() const
+bool lfu::LFUCache<K, V>::empty() const
 {
     return key_map_.empty();
 }
 
 template<typename K, typename V>
-size_t LFUCache<K, V>::capacity() const
+size_t lfu::LFUCache<K, V>::capacity() const
 {
     return capacity_;
 }
 
 template<typename K, typename V>
-void LFUCache<K, V>::clear()
+void lfu::LFUCache<K, V>::clear()
 {
     frequency_map_.clear();
     key_map_.clear();
